677f36136ffb0c766bd574ef7408f015731d8672
Harrison Ingram-Bate <harrison.ingrambate@gmail.com>
2026-01-26 07:41:10 -0800

    added doxygen comments to Population.h

--- Lab1/src/Population.h
+++ Lab1/src/Population.h
@@ -6,6 +6,11 @@ using namespace std;
 
 class Population {
 public: 
+	/**
+	* Constructor for Population initializes population matrix to be of size pop_size x dimensions
+	* and fitness vector to size pop_size
+	* @param[in] pop_size, dimensions
+	*/
 	Population(size_t pop_size, size_t dimensions);
 
 	vector<vector<float>> population;
