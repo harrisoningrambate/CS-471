e0b0e7ef8fcac3d9cf5ba7637e3d57efe793d7fe
Harrison Ingram-Bate <harrison.ingrambate@gmail.com>
2026-01-26 07:44:32 -0800

    added doxygen comments to Problem.h

--- Lab1/src/Problem.h
+++ Lab1/src/Problem.h
@@ -3,23 +3,73 @@
 #include <vector>
 using namespace std;
 
+/**
+* Calculates the fitness of a vector evaluated with Schwefel's function.
+* @param[in] vec
+* @param[out] fitness
+*/
 float Schwefel(const vector<float>& vec);
 
+/**
+* Calculates the fitness of a vector evaluated with 1st De Jong's function.
+* @param[in] vec
+* @param[out] fitness
+*/
 float FirstDeJong(const vector<float>& vec);
 
+/**
+* Calculates the fitness of a vector evaluated with Rosenbrock's Sadle.
+* @param[in] vec
+* @param[out] fitness
+*/
 float Rosenbrock(const vector<float>& vec);
 
+/**
+* Calculates the fitness of a vector evaluated with Rastrigin.
+* @param[in] vec
+* @param[out] fitness
+*/
 float Rastrigin(const vector<float>& vec);
 
+/**
+* Calculates the fitness of a vector evaluated with Griewangk.
+* @param[in] vec
+* @param[out] fitness
+*/
 float Griewangk(const vector<float>& vec);
 
+/**
+* Calculates the fitness of a vector evaluated with Sine Envelope Sine Wave.
+* @param[in] vec
+* @param[out] fitness
+*/
 float SineEnvelope(const vector<float>& vec);
 
+/**
+* Calculates the fitness of a vector evaluated with Stretched V Sine Wave.
+* @param[in] vec
+* @param[out] fitness
+*/
 float StretchedV(const vector<float>& vec);
 
+/**
+* Calculates the fitness of a vector evaluated with Ackley's One.
+* @param[in] vec
+* @param[out] fitness
+*/
 float AckleyOne(const vector<float>& vec);
 
+/**
+* Calculates the fitness of a vector evaluated with Ackley's Two
+* @param[in] vec
+* @param[out] fitness
+*/
 float AckleyTwo(const vector<float>& vec);
 
+/**
+* Calculates the fitness of a vector evaluated with Egg Holder.
+* @param[in] vec
+* @param[out] fitness
+*/
 float EggHolder(const vector<float>& vec);
 #endif
