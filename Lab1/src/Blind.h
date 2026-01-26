dc9db604a2a138e6dc6b92a4babf0258c522f7aa
Harrison Ingram-Bate <harrison.ingrambate@gmail.com>
2026-01-26 07:33:26 -0800

    added doxygen comments to Blind.h

--- Lab1/src/Blind.h
+++ Lab1/src/Blind.h
@@ -6,6 +6,11 @@
 #include <chrono>
 #include "Population.h"
 
+/**
+* Runs Blind Algorithm on a fitness function for pop_size iterations.
+* @param[in] distribution_vec, fitness function, pop_size
+* @param[out] Population (contains randomly generated population and calculated fitness vector)
+*/
 Population Blind(std::unique_ptr<vector<uniform_real_distribution<float>>>  distribution_vec,
 				 float (*fitness)(const vector<float>&),
 				 size_t pop_size);
