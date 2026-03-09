c887457c99c79c66fab28cfeed999c16775149a7
Harrison Ingram-Bate <harrison.ingrambate@gmail.com>
2026-03-08 18:37:47 -0700

    implemented FShop class to hold a Flow Shop with a makespan value and a fp to calculate the makespan

--- Project3/src/Population.cpp
+++
@@ -1,4 +0,0 @@
-// Name: Harrison Ingram-Bate
-#include "Population.h"
-
-Population::Population(size_t pop_size, size_t dimensions): population(pop_size, vector<float>(dimensions)), fitness(pop_size) {};
